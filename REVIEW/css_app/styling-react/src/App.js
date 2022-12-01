5fe3b6f68a3ccd10a5c59f9275e04d1da1802a5f
Phantasia0 <ncprog1@gmail.com>
2022-12-01 19:38:45 +0900

    Start

--- REVIEW/css_app/styling-react/src/App.js
+++ REVIEW/css_app/styling-react/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponents";
 
 class App extends Component {
   render() {
     return (
       <div>
         <SassComponent />
+        <CSSModule />
+        <StyledComponent />
       </div>
     );
   }
