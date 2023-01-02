cb258919f2caadc7fc85d9bb7fe88b8df5a56ed9
LAPTOP-BMARPJVV\PC <ncprog1@gmail.com>
2023-01-03 07:35:01 +0900

    Spliting Ways

--- REVIEW/splitting-sample/src/App.js
+++ REVIEW/splitting-sample/src/App.js
@@ -1,22 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// class App extends Component {
+//   state = {
+//     SplitMe: null,
+//   };
+
+//   handleClick = async () => {
+//     const loadedModule = await import("./SplitMe");
+//     this.setState({
+//       SplitMe: loadedModule.default,
+//     });
+//   };
+
+//   render() {
+//     const { SplitMe } = this.state;
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} classname="App-logo" alt="logo" />
+//           <p onClick={this.handleClick}>Hello React!</p>
+//           {SplitMe && <SplitMe />}
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+/*
+  React.lazy Suspense 이용한 컴포넌트 코드 스플리팅
+*/
+
+// import React from "react";
+// import { useState, Suspense } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// const SplitMe = React.lazy(() => import("./SplitMe"));
+
+// function App() {
+//   const [visible, setVisible] = useState(false);
+//   const onClick = () => {
+//     setVisible(true);
+//   };
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p onClick={onClick}>Hello React!</p>
+//         <Suspense fallback={<div>loading...</div>}>
+//           {visible && <SplitMe />}
+//         </Suspense>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/*
+  Loadable Components 를 통한 코드 스플리팅
+*/
+
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
