1e2da42f7b67c692735daf8384803f17b017f0bf
LAPTOP-BMARPJVV\PC <ncprog1@gmail.com>
2023-01-04 09:40:48 +0900

    Moving PC

---
+++ REVIEW/ssr-recipe (thunk)/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
