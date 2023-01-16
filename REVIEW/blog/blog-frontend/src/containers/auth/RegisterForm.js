54e33da071a45703712da9450f251a5ee03d165d
User -2023CQCJY\user <ncprog1@gmail.com>
2023-01-16 20:54:09 +0900

    RLL finished

--- REVIEW/blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ REVIEW/blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -70,7 +70,12 @@ const RegisterForm = () => {
 
   useEffect(() => {
     if (user) {
-      navigate('/'); //홈화면으로 이동
+      navigate('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [navigate, user]);
 
